0d6a1f71d179ebb7c331cf2d8d3523be25146ef4
AndersonFSP <anfisope10@hotmail.com>
2022-07-23 17:49:03 -0300

    feat: action teste

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 const firebaseAuth = getAuth(app)
+const analytics = getAnalytics(app)
 export { app, analytics, firebaseAuth }
